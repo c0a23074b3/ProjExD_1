5a0c1fa70b78ba4765dd1ac100d117de946ab583
satoukibi <c0a23074b3@edu.yeu.ac.jp>
2024-04-16 17:25:21 +0900

    lec8

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,32 @@ def main():
     kk_img = pg.image.load("fig/3.png") #練習２
     kk_img = pg.transform.flip(kk_img, True, False)#練習２
     tbg_img = pg.transform.flip(bg_img, True, False)#練習７
+    kk_img_rct = kk_img.get_rect()#練習８-1こうかとんrectを抽出
+    kk_img_rct.center = 300, 200#こうかとんrectの指定
+    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        #print(key_lst) 
+        if key_lst[pg.K_UP]:
+            kk_img_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_img_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_img_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_img_rct.move_ip((1,0))
+
         x = tmr%3200#練習６（背景画像繰り返すための処理）
         screen.blit(bg_img, [-x, 0])
         screen.blit(tbg_img,[(-x+1600),0])#練習７
         screen.blit(bg_img, [(-x+3200),0])
         screen.blit(tbg_img,[(-x+4800),0])
-        screen.blit(kk_img, [300, 200])#練習４
-        print(tmr,x)
+        screen.blit(kk_img, kk_img_rct)
+        #print(tmr,x)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
